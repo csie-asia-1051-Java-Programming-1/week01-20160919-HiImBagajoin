c6809ff664b9c9b141ab1bc3c0896109a0204a93
He Yuan Hao <104021051@live.asia.edu.tw>
2016-09-19 15:37:15 +0800

    parctice 1

--- week01_20160919/src/ex/ex02_1050210XX.java
+++ week01_20160919/src/ex/ex02_1050210XX.java
@@ -4,12 +4,14 @@ package ex;
  * Date: 2016/09/19
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.Scanner;
 public class ex02_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn=new Scanner(System.in);
+		int a=scn.nextInt();
+		System.out.println((a*0.45359)+"磅");
 	}
 
 }
